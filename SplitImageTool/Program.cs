7931c8792d1ef810c19b1bfba1ed13552000f29f
pengkun <pengkun.hb@gmail.com>
2015-11-13 17:44:20 +0800

    create

---
+++ SplitImageTool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplitImage.Core;
+
+namespace SplitImageTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SplitImageClass.SplitImageDiretory(@"D:\work\3xrpg\美术\正式资源\特效\new", 4, 4);
+            SplitImageClass.SplitImageFile(@"D:\work\3xrpg\美术\正式资源\特效\new\etherBall.png", 8, 4);
+        }
+    }
+}
